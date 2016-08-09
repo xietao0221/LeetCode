68e102dadd9f08bfcb82901727d757cc3c860c96
Tao Xie <taoxie@usc.edu>
2016-08-08 19:07:16 -0700

    0808

--- 315. Count of Smaller Numbers After Self.java
+++ 315. Count of Smaller Numbers After Self.java
@@ -1,3 +1,40 @@
+// binary indexed tree
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        if(nums == null || nums.length == 0) return new ArrayList<>();
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++) min = Math.min(min, nums[i]);;
+        for(int i=0; i<nums.length; i++) {
+            nums[i] = nums[i] - min + 1;
+            max = Math.max(max, nums[i]);
+        }
+        List<Integer> res = new ArrayList<>();
+        int[] fenwickTree = new int[max + 1];
+        for(int i=nums.length-1; i>=0; i--) {
+            res.add(0, getSum(fenwickTree, nums[i]-1));
+            updateFenwickTree(fenwickTree, nums[i], 1);
+        }
+        return res;
+    }
+    
+    private void updateFenwickTree(int[] fenwickTree, int index, int value) {
+        while(index < fenwickTree.length) {
+            fenwickTree[index] += value;
+            index += (index & -index);
+        }
+    }
+    
+    private int getSum(int[] fenwickTree, int index) {
+        int sum = 0;
+        while(index > 0) {
+            sum += fenwickTree[index];
+            index -= (index & -index);
+        }
+        return sum;
+    }
+}
+
+/*
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> visited = new ArrayList<>();
@@ -25,3 +62,4 @@ public class Solution {
         return start;
     }
 }
+*/
