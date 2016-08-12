ba7155a9a8f494b727697d23e4a122b47b7a79a2
Tao Xie <taoxie@usc.edu>
2016-08-11 23:29:14 -0700

    0811

---
+++ 132. Palindrome Partitioning II.java
@@ -0,0 +1,68 @@
+// https://discuss.leetcode.com/topic/32575/easiest-java-dp-solution-97-36
+public class Solution {
+    public int minCut(String s) {
+        // cut[i] is the min-cut of string ending at i
+        int[] cut = new int[s.length()];
+        
+        // palindrome[i][j] represents whether s.substring(i, j+1) is palindrome
+        boolean[][] palindrome = new boolean[s.length()][s.length()];
+        
+        // end:    [0, s.length()-1]
+        // start:  [0, end]
+        for(int end=0; end<=s.length()-1; end++) {
+            // if the length of string is end+1, the max-cut is end
+            int min = end;
+            for(int start=0; start<=end; start++) {
+                if(s.charAt(start) == s.charAt(end) && (start+1 > end-1 || palindrome[start+1][end-1])) {
+                    palindrome[start][end] = true;
+                    
+                    if(start == 0) min = 0;
+                    else min = Math.min(min, 1 + cut[start-1]);
+                }
+            }
+            cut[end] = min;
+        }
+        return cut[s.length()-1];
+    }
+}
+
+
+// https://www.youtube.com/watch?v=lDYIvtBVmgo
+// This solution results in TLE, just read it and don't use it.
+/*
+public class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[][] cut = new int[n][n], palindrome = new int[n][n];
+        char[] sArray = s.toCharArray();
+        
+        for(int len=0; len<n; len++) {
+            for(int start=0; start+len<n; start++) {
+                int end = start + len;
+                if(palindrome[start][end] == 1 || isPalindrome(sArray, start, end, palindrome)) {
+                    cut[start][end] = 0;
+                } else {
+                    int localMin = Integer.MAX_VALUE;
+                    for(int mid=start; mid<end; mid++) {
+                        localMin = Math.min(localMin, cut[start][mid] + cut[mid+1][end]);
+                    }
+                    cut[start][end] = 1 + localMin;
+                }
+            }
+        }
+        return cut[0][n-1];
+    }
+    
+    public boolean isPalindrome(char[] sArray, int start, int end, int[][] palindrome) {
+        int i = start, j = end;
+        while(start <= end) {
+            if(sArray[start++] != sArray[end--]) {
+                palindrome[i][j] = -1;
+                return false;
+            }
+        }
+        palindrome[i][j] = 1;
+        return true;
+    }
+}
+*/
---
+++ 336. Palindrome Pairs.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        if(words == null || words.length < 2) return res;
+        
+        // save all words in the map
+        for(int i=0; i<words.length; i++) map.put(words[i], i);
+        
+        // iterate the words
+        for(int i=0; i<words.length; i++) {
+            for(int j=0; j<=words[i].length(); j++) {
+                String part1 = words[i].substring(0, j), part2 = words[i].substring(j);
+                // check part1, part1 could be [0, words[i].length()]
+                if(isPalindrome(part1)) {
+                    String part2Reverse = new StringBuilder(part2).reverse().toString();
+                    if(map.containsKey(part2Reverse) && map.get(part2Reverse) != i) {
+                        res.add(new ArrayList<>(Arrays.asList(map.get(part2Reverse), i)));
+                    }
+                }
+                
+                // check part2, part2 could not be empty, so if part2 is empty, continue
+                if(isPalindrome(part2)) {
+                    String part1Reverse = new StringBuilder(part1).reverse().toString();
+                    if(map.containsKey(part1Reverse) && map.get(part1Reverse) != i && part2.length() != 0) {
+                        res.add(new ArrayList<>(Arrays.asList(i, map.get(part1Reverse))));
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length()-1;
+        while(start < end) {
+            if(s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
