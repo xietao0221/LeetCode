c6061a64d398ede94e524930dcaf0f01c59fad0f
Tao Xie <taoxie@usc.edu>
2016-08-13 14:24:54 -0700

    0813

---
+++ 66. Plus One.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] newDigits = new int[digits.length + 1];
+        newDigits[0] = 1;
+        for(int i=0; i<digits.length; i++) newDigits[i+1] = digits[i];
+        return newDigits;
+    }
+}
