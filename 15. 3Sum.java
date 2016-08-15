197c5324bfd389dae381b36d4c6e4e86d7ac4714
Tao Xie <taoxie@usc.edu>
2016-08-14 22:12:02 -0700

    0814

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -1,3 +1,4 @@
+// Iterative Approach
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         if(nums.length < 3) return new ArrayList<>();
@@ -23,3 +24,33 @@ public class Solution {
         return list;
     }
 }
+
+
+// Recursive Approach
+/*
+public class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<Integer> tmpRes = new ArrayList<>();
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        if(nums == null || nums.length < 3) return res;
+        Arrays.sort(nums);
+        threeSumHelper(nums, 0, 3, 0);
+        return res;
+    }
+    
+    private void threeSumHelper(int[] nums, int pos, int count, int sum) {
+        if(pos == nums.length || count == 0) {
+            if(sum == 0 && count == 0) res.add(new ArrayList<>(tmpRes));
+            return;
+        }
+        
+        for(int i=pos; i<nums.length && (sum + nums[i] <= 0); i++) {
+            if(i > pos && nums[i] == nums[i-1]) continue;
+            tmpRes.add(nums[i]);
+            threeSumHelper(nums, i + 1, count - 1, sum + nums[i]);
+            tmpRes.remove(tmpRes.size() - 1);
+        }
+    }
+}
+*/
--- 78. Subsets.java
+++ 78. Subsets.java
@@ -1,3 +1,25 @@
+// Recursive Approach
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        subsetsHelper(nums, 0, res, new ArrayList<>());
+        return res;
+    }
+    
+    private void subsetsHelper(int[] nums, int pos, List<List<Integer>> res, List<Integer> tmpRes) {
+        if(pos <= nums.length) res.add(new ArrayList<>(tmpRes));
+        
+        for(int i=pos; i<nums.length; i++) {
+            tmpRes.add(nums[i]);
+            subsetsHelper(nums, i + 1, res, tmpRes);
+            tmpRes.remove(tmpRes.size() - 1);
+        }
+    }
+}
+
+// Iterative Approach
+/*
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         Arrays.sort(nums);
@@ -5,14 +27,15 @@ public class Solution {
         // empty list
         res.add(new ArrayList<>());
         // for each old list, add a new element
-        for(int num: nums) {
+        for(int i=0; i<nums.length; i++) {
             int size = res.size();
-            for(int i=0; i<size; i++) {
-                List<Integer> newList = new ArrayList<>(res.get(i));
-                newList.add(num);
+            for(int j=0; j<size; j++) {
+                List<Integer> newList = new ArrayList<>(res.get(j));
+                newList.add(nums[i]);
                 res.add(newList);
             }
         }
         return res;
     }
 }
+*/
