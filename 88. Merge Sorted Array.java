7b6fbb60a96f5aebb555e20f3a9a92fa7ffa293c
Tao Xie <taoxie@usc.edu>
2016-08-14 17:11:21 -0700

    0814

---
+++ 88. Merge Sorted Array.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m - 1, index2 = n - 1, anchor = m + n - 1;
+        while(index1 >= 0 && index2 >= 0) {
+            if(nums1[index1] >= nums2[index2]) nums1[anchor--] = nums1[index1--];
+            else nums1[anchor--] = nums2[index2--];
+        }
+        while(index2 >= 0) nums1[anchor--] = nums2[index2--];
+    }
+}
