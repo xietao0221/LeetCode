9f734cea106e14d5c24281340df2c8a253b5fd2a
Tao Xie <taoxie@usc.edu>
2016-08-17 12:01:42 -0700

    0817

---
+++ 3. Longest Substring Without Repeating Characters.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/discuss/72701/here-10-line-template-that-can-solve-most-substring-problems
+// Sliding Window
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] charSet = new int[128];
+        int left = 0, right = 0, res = 0;
+        while(right < s.length()) {
+            // normally, the count could only be 0 and 1, if it is larger than 1
+            // we need to shrink the window size(increase the left) to remove the duplicates
+            if(++charSet[s.charAt(right++)] > 1) {
+                while(true) {
+                    // normally, after --, the value can only be 0, if it is larger than 0
+                    // this one is duplicate, we stop shrinking
+                    if(--charSet[s.charAt(left++)] > 0) break;
+                }
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
+
+// Hash Table
+/*
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, res = 0;
+        while(right < s.length()) {
+            if(set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left++));
+            } else {
+                set.add(s.charAt(right++));
+                res = Math.max(res, right - left);
+            }
+        }
+        return res;
+    }
+}
+*/
