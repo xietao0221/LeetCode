21d6dc564cfbc3c36d90c1b6e91167d9a197db4d
Tao Xie <taoxie@usc.edu>
2016-08-16 17:21:52 -0700

    0816

---
+++ 238. Product of Array Except Self.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        
+        // from left to right, for each num[i], calculate the product from 0 to i-1
+        int left = 1;
+        for(int i = 1; i < res.length; i++) {
+            left *= nums[i-1];
+            res[i] = left;
+        }
+        
+        // from right to left, for each num[i], calculate the product from n-1 to i+1
+        int right = 1;
+        for(int i = res.length - 2; i >= 0; i--) {
+            right *= nums[i+1];
+            res[i] *= right; 
+        }
+        return res;
+    }
+}
