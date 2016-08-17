7ae1f64e2f94ecf6251f63e82e2dacb6b2b77d0c
Tao Xie <taoxie@usc.edu>
2016-08-16 19:06:49 -0700

    0816

--- 259. 3Sum Smaller.java
+++ 259. 3Sum Smaller.java
@@ -2,8 +2,8 @@ public class Solution {
     public int threeSumSmaller(int[] nums, int target) {
         Arrays.sort(nums);
         int count = 0;
-        for(int i=0; i<nums.length; i++) {
-            int left = i+1, right = nums.length-1;
+        for(int i = 0; i < nums.length; i++) {
+            int left = i + 1, right = nums.length - 1;
             while(left < right) {
                 if(nums[i] + nums[left] + nums[right] < target) {
                     count += right - left;
