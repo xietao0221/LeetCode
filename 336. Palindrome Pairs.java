4bde299c7be4639e6554b4bc761ed9c084ea15cf
Tao Xie <taoxie@usc.edu>
2016-08-24 11:18:45 -0700

    0824

--- 336. Palindrome Pairs.java
+++ 336. Palindrome Pairs.java
@@ -33,7 +33,7 @@ public class Solution {
     }
     
     public boolean isPalindrome(String s) {
-        int start = 0, end = s.length()-1;
+        int start = 0, end = s.length() - 1;
         while(start < end) {
             if(s.charAt(start++) != s.charAt(end--)) return false;
         }
