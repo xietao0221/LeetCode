e45b2955fbf5742f0f842d3a1105c1260de7a6c0
Tao Xie <taoxie@usc.edu>
2016-08-24 20:38:29 -0700

    0824

--- 215. Kth Largest Element in an Array.java
+++ 215. Kth Largest Element in an Array.java
@@ -1,5 +1,7 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
+        // the default heap is min-heap, the top of heap is min, queue.peek(), queue.poll() is the min
+        // this heap always keep the k largest number, and queue.peek() is the smallest in these k numbers
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         for(int n: nums) {
             queue.offer(n);
--- 240. Search a 2D Matrix II.java
+++ 240. Search a 2D Matrix II.java
@@ -4,7 +4,7 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = 0, col = matrix[0].length - 1;
-        while(row <= matrix.length-1 && col >= 0) {
+        while(row <= matrix.length - 1 && col >= 0) {
             if(matrix[row][col] == target) return true;
             else if(matrix[row][col] > target) col--;
             else row++;
