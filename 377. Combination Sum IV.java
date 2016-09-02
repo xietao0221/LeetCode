e950b494b664bb8a95f764dcbbabde3b3dd63b9e
Tao Xie <taoxie@usc.edu>
2016-09-02 16:44:37 -0700

    0902

--- 377. Combination Sum IV.java
+++ 377. Combination Sum IV.java
@@ -6,10 +6,10 @@ public class Solution {
     public int combinationSum4(int[] nums, int target) {
         int[] dp = new int[target + 1];
         dp[0] = 1;
-        for (int i=1; i<=target; i++) {
+        for (int i = 1; i <= target; i++) {
             // comb[target] = sum(comb[target - nums[j]])
             // if only need one more number to reach target, this number could be any one in the array
-            for (int j=0; j<nums.length; j++) {
+            for (int j = 0; j < nums.length; j++) {
                 if (i >= nums[j]) {
                     dp[i] += dp[i - nums[j]];
                 }
@@ -29,13 +29,15 @@ public class Solution {
     public int combinationSum4(int[] nums, int target) {
         if(target == 0) return 1;
         if(memo.containsKey(target)) return memo.get(target);
+        
         int res = 0;
-        for(int i=0; i<nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
             if(target >= nums[i]) {
                 res += combinationSum4(nums, target - nums[i]);
             }
         }
         memo.put(target, res);
+        
         return res;
     }
 }
