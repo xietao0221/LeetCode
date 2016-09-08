b4c629b9f6e5ceab94ded4aa6a2a690e4f68452c
Tao Xie <taoxie@usc.edu>
2016-09-07 23:45:39 -0700

    0907

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -1,14 +1,45 @@
+// Recursive Solution
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        permuteHelper(nums, new boolean[nums.length], res, new ArrayList<>());
+        return res;
+    }
+    
+    private void permuteHelper(int[] nums, boolean[] used, List<List<Integer>> res, List<Integer> tmpRes) {
+        if(tmpRes.size() == nums.length) {
+            res.add(new ArrayList<>(tmpRes));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(used[i]) continue;
+            
+            used[i] = true;
+            tmpRes.add(nums[i]);
+            
+            permuteHelper(nums, used, res, tmpRes);
+            
+            used[i] = false;
+            tmpRes.remove(tmpRes.size() - 1);
+        }
+    }
+}
+
+// Iterative Solution
+/*
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> lists = new ArrayList<>();
         if(nums == null || nums.length == 0) return lists;
         
         lists.add(new ArrayList<>(Arrays.asList(nums[0])));
-        for(int i=1; i<nums.length; i++) {                  // the new element to be added
+        for(int i = 1; i < nums.length; i++) {                  // the new element to be added
             List<List<Integer>> newLists = new ArrayList<>();
-            for(int j=0; j<lists.size(); j++) {             // how many old list to be modified
+            for(int j = 0; j < lists.size(); j++) {             // how many old list to be modified
                 List<Integer> oldList = lists.get(j);
-                for(int k=0; k<=i; k++) {                   // for each old list, how many position can be inserted
+                for(int k = 0; k <= i; k++) {                   // for each old list, how many position can be inserted
                     List<Integer> newList = new ArrayList<>(oldList);
                     newList.add(k, nums[i]);
                     newLists.add(newList);
@@ -19,3 +50,4 @@ public class Solution {
         return lists;
     }
 }
+*/
--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -10,21 +10,22 @@ public class Solution {
     public void permuteUniqueHelper(int[] nums, List<List<Integer>> res, List<Integer> tmpList, boolean[] used) {
         if(tmpList.size() == nums.length) {
             res.add(new ArrayList<>(tmpList));
-        } else {
-            for(int i=0; i<nums.length; i++) {
-                // if the current element equals to the previous one, and we don't choose the previous one, continue
-                // if we have choose this one, continue (because each round, we iterate the whole array)
-                if((i > 0 && nums[i] == nums[i-1] && !used[i-1]) || used[i]) continue;
-                
-                used[i] = true;
-                tmpList.add(nums[i]);
-                
-                permuteUniqueHelper(nums, res, tmpList, used);
-                
-                // backtracking
-                tmpList.remove(tmpList.size() - 1);
-                used[i] = false;
-            }
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            // if the current element equals to the previous one, and we don't choose the previous one, continue
+            // if we have choose this one, continue (because each round, we iterate the whole array)
+            if((i > 0 && nums[i] == nums[i-1] && !used[i-1]) || used[i]) continue;
+            
+            used[i] = true;
+            tmpList.add(nums[i]);
+            
+            permuteUniqueHelper(nums, res, tmpList, used);
+            
+            // backtracking
+            tmpList.remove(tmpList.size() - 1);
+            used[i] = false;
         }
     }
 }
@@ -37,12 +38,12 @@ public class Solution {
         if(nums == null || nums.length == 0) return list;
         list.add(new ArrayList<>(Arrays.asList(nums[0])));
         
-        for(int i=1; i<nums.length; i++) {
+        for(int i = 1; i < nums.length; i++) {
             HashSet<List<Integer>> set = new HashSet<>();   
             List<List<Integer>> newList = new ArrayList<>();
-            for(int j=0; j<list.size(); j++) {
+            for(int j = 0; j < list.size(); j++) {
                 List<Integer> oldList = list.get(j);
-                for(int k=0; k<=i; k++) {
+                for(int k = 0; k <= i; k++) {
                     List<Integer> subList = new ArrayList<>(oldList);
                     subList.add(k, nums[i]);
                     if(set.add(subList)) newList.add(subList);      //similar to permutation I, use a hashset to prune
