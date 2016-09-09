a563dbf05e9e301a4e7488e90753055b137dde56
Tao Xie <taoxie@usc.edu>
2016-09-08 21:46:39 -0700

    0908

--- 128. Longest Consecutive Sequence.java
+++ 128. Longest Consecutive Sequence.java
@@ -1,3 +1,4 @@
+// hashset
 public class Solution {
     public int longestConsecutive(int[] nums) {
         int res = 0;
@@ -20,3 +21,64 @@ public class Solution {
         return res;
     }
 }
+
+// union and find
+/*
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        UnionFind set = new UnionFind(nums.length);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) continue;
+            if(map.containsKey(nums[i] + 1)) set.union(i, map.get(nums[i] + 1));
+            if(map.containsKey(nums[i] - 1)) set.union(i, map.get(nums[i] - 1));
+            map.put(nums[i], i);
+        }
+        return set.size();
+    }
+    
+    class UnionFind {
+        int[] id, size;
+        int count;
+        
+        public UnionFind(int length) {
+            id = new int[length];
+            size = new int[length];
+            for(int i = 0; i < length; i++) {
+                id[i] = i;
+                size[i] = 1;
+            }
+            count = 1;
+        }
+        
+        public int size() {
+            return count;
+        }
+        
+        private int root(int i) {
+            while(i != id[i]) {
+                id[i] = id[id[i]];
+                i = id[i];
+            }
+            return i;
+        }
+        
+        public void union(int p, int q) {
+            int pi = root(p), qi = root(q);
+            if(pi == qi) return;
+            
+            int localCount = 0;
+            if(size[pi] < size[qi]) {
+                id[pi] = qi;
+                size[qi] += size[pi];
+                localCount = size[qi];
+            } else {
+                id[qi] = pi;
+                size[pi] += size[qi];
+                localCount = size[pi];
+            }
+            count = Math.max(count, localCount);
+        }
+    }
+}
+*/
