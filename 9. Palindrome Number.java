d27cb0dcb64daa44f5d9ba8523b1c2d66215698b
Tao Xie <taoxie@usc.edu>
2016-09-20 12:07:57 -0700

    0920

--- 9. Palindrome Number.java
+++ 9. Palindrome Number.java
@@ -1,14 +1,15 @@
 public class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
+        
         int len = 1, temp = x;
         while(temp > 9) {
             temp = temp / 10;
             len++;
         }
-
-        for(int i=0; i<len/2; i++) {
-            if(getIthDigit(x, len, i) != getIthDigit(x, len, len-1-i)) return false;
+        
+        for(int i = 0; i < len / 2; i++) {
+            if(getIthDigit(x, len, i) != getIthDigit(x, len, len - 1 - i)) return false;
         }
         return true;
     }
