fc4a556c0f3c3ab609730dbdddd52fac32dde808
Tao Xie <taoxie@usc.edu>
2016-09-22 16:45:03 -0700

    0922

--- 260. Single Number III.java
+++ 260. Single Number III.java
@@ -1,5 +1,20 @@
+// hash set solution
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n: nums) {
+            if(set.contains(n)) set.remove(n);
+            else set.add(n);
+        }
+        
+        Iterator iter = set.iterator();
+        return new int[]{(int)iter.next(), (int)iter.next()};
+    }
+}
+
 // bit manipulation solution
 // https://discuss.leetcode.com/topic/21605/accepted-c-java-o-n-time-o-1-space-easy-solution-with-detail-explanations
+/*
 public class Solution {
     public int[] singleNumber(int[] nums) {
         int diff = 0;
@@ -17,21 +32,6 @@ public class Solution {
             else res[1] ^= num;
         }
         return res;
-    }
-}
-
-// hash set solution
-/*
-public class Solution {
-    public int[] singleNumber(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int n: nums) {
-            if(set.contains(n)) set.remove(n);
-            else set.add(n);
-        }
-        
-        Iterator iter = set.iterator();
-        return new int[]{(int)iter.next(), (int)iter.next()};
     }
 }
 */
