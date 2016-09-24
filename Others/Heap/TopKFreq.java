c9fb761193d1eab55ca0ebaf63adae55ff004af8
Tao Xie <taoxie@usc.edu>
2016-09-24 00:25:11 -0700

    0924

--- Others/Heap/TopKFreq.java
+++ Others/Heap/TopKFreq.java
@@ -4,33 +4,46 @@ Given a stream of String, output top 10 frequency Strings
 public class TopKFreq {
     Map<String, Integer> map;
     PriorityQueue<NameFreq> queue;
+    Set<String> set;
     int size;
 
     public TopKFreq(int size) {
         map = new HashMap<>();
         queue = new PriorityQueue<>(size, (a, b) -> a.freq - b.freq);
+        set = new HashSet<>();
         this.size = size;
     }
 
-    public List<String> add(String name) {
+    public List<NameFreq> add(String name) {
         if(!map.containsKey(name)) map.put(name, 1);
         else map.put(name, map.get(name) + 1);
 
+        boolean isDup = set.contains(name);
+
+        List<NameFreq> res = new ArrayList<>();
         NameFreq curr = new NameFreq(name, map.get(name));
         if(queue.size() < size) {
-            queue.offer(curr);
-        } else {
-            if(queue.peek().freq < map.get(name)) {
-                queue.poll();
+            if(!isDup) {
                 queue.offer(curr);
+                set.add(name);
+            }
+        } else {
+            if(queue.peek().freq < map.get(name) && !isDup) {
+                set.remove(queue.poll().name);
+                queue.offer(curr);
+                set.add(name);
             }
         }
 
-        List<String> res = new ArrayList<>();
-        PriorityQueue<NameFreq> newQueue = new PriorityQueue<>(size, (a, b) -> a.freq - b.freq);
+        PriorityQueue<NameFreq> newQueue = new PriorityQueue<>((a, b) -> a.freq - b.freq);
+        if(isDup) queue.offer(curr);
         while(!queue.isEmpty()) {
             NameFreq tmp = queue.poll();
-            res.add(0, tmp.name);
+            if(isDup && tmp.name.equals(name)) {
+                isDup = false;
+                continue;
+            }
+            res.add(0, tmp);
             newQueue.offer(tmp);
         }
         queue = newQueue;
@@ -44,6 +57,10 @@ public class TopKFreq {
         public NameFreq(String name, int freq) {
             this.name = name;
             this.freq = freq;
+        }
+
+        public String toString() {
+            return name + ":" + freq;
         }
     }
 }
