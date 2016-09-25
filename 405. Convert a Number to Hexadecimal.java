f4c69c88e87bcf41734fe717554de2668c939b42
Tao Xie <taoxie@usc.edu>
2016-09-25 00:45:17 -0700

    0925

---
+++ 405. Convert a Number to Hexadecimal.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public String toHex(int num) {
+        if(num == 0) return "0";
+        
+        char[] map = new char[]{'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        StringBuilder sb = new StringBuilder();
+        while(num != 0) {
+            sb.insert(0, map[num & 15]);
+            num >>>= 4;
+        }
+        return sb.toString();
+    }
+}
