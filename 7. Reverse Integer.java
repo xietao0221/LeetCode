77f4e80d27804bcfdd80f1241a6f2b8516630e02
Tao Xie <taoxie@usc.edu>
2016-10-01 01:41:44 -0700

    1001

--- 7. Reverse Integer.java
+++ 7. Reverse Integer.java
@@ -1,12 +1,39 @@
+// Int Solution
 public class Solution {
     public int reverse(int x) {
-        long res = 0;
+        boolean sign = x >= 0;
+        x = Math.abs(x);
+        int res = 0;
+        
         while(x != 0) {
-            int tail = x % 10;      // if x < 0, x % 10 < 0
-            res = res * 10 + tail;
-            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+            int tmpRes = res * 10 + x % 10;
             x /= 10;
+            
+            // handle overflow, have to with the help of temporary int
+            if(tmpRes / 10 != res) return 0;
+            res = tmpRes;
         }
-        return (int)res;
+        return sign ? res : -res;
     }
 }
+
+// Long Solution
+/*
+public class Solution {
+    public int reverse(int x) {
+        boolean sign = x >= 0;
+        x = Math.abs(x);
+
+        long res = 0;
+        while(x != 0) {
+            // get the last digits. if x < 0, x % 10 < 0
+            int tail = x % 10;
+            x /= 10;
+            
+            res = res * 10 + tail;
+            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+        }
+        return sign ? (int)res : -(int)res;
+    }
+}
+*/
--- 8. String to Integer (atoi).java
+++ 8. String to Integer (atoi).java
@@ -1,35 +1,31 @@
 public class Solution {
     public int myAtoi(String str) {
         if(str == null || str.length() == 0) return 0;
+        char[] sArray = str.trim().toCharArray();
+        if(sArray.length == 0) return 0;
         
-        char[] strArray = str.toCharArray();
-        int index = 0, sign = 1;
-        long res = 0;
-        
-        // remove leading zeroes
-        while(strArray[index] == ' ' && index < strArray.length) index++;
+        int index = 0, sign = 1, res = 0;
         
         // deal with sign
-        if(strArray[index] == '+' || strArray[index] == '-') {
-            sign = strArray[index] == '+' ? 1 : -1;
+        if(sArray[index] == '+' || sArray[index] == '-') {
+            sign = sArray[index] == '+' ? 1 : -1;
             index++;
         }
         
-        while(index < strArray.length) {
-            int digit = strArray[index] - '0';
+        while(index < sArray.length) {
+            int digit = sArray[index] - '0';
             
             // deal with bad input
             if(digit < 0 || digit > 9) break;
             
-            res = 10 * res + digit;
+            int tmp = 10 * res + digit;
             
             // deal with overflow
-            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
-                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
+            if(tmp / 10 != res) return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             
             index++;
+            res = tmp;
         }
-        return (int)res * sign;
+        return sign == 1 ? res : -res;
     }
 }
