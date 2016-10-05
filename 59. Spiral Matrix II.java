39546edda3d79c1a73cb9de06934426781588caf
Tao Xie <taoxie@usc.edu>
2016-10-04 17:22:50 -0700

    1004

--- 59. Spiral Matrix II.java
+++ 59. Spiral Matrix II.java
@@ -1,21 +1,21 @@
-// like '48. Rotate Image'
+// similar to '48. Rotate Image'
 public class Solution {
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
         if(n == 0) return result;
         int i = 1;
-        for(int row=0; row<n/2; row++) {
-            int colSize = n - row - 1;
-            for(int col=row; col<colSize; col++) {
+        for(int row = 0; row < n / 2; row++) {
+            int colSize = n - row - 1, gap = colSize - row;
+            for(int col = row; col < colSize; col++) {
                 result[row][col] = i;
-                result[col][n-1-row] = i + (colSize - row);
-                result[n-1-row][n-1-col] = i + 2 * (colSize - row);
-                result[n-1-col][row] = i + 3 * (colSize - row);
+                result[col][n - 1 - row] = i + gap;
+                result[n - 1 - row][n - 1 - col] = i + 2 * gap;
+                result[n - 1 - col][row] = i + 3 * gap;
                 i++;
             }
-            i += 3 * (colSize - row);
+            i += 3 * gap;
         }
-        if(n % 2 != 0) result[n/2][n/2] = n * n;
+        if(n % 2 != 0) result[n / 2][n / 2] = n * n;
         return result;
     }
 }
