4279cbe9134e7535c5f12cc78f9582d2e24ecfcc
Tao Xie <taoxie@usc.edu>
2016-10-10 12:43:44 -0700

    1010

--- 163. Missing Ranges.java
+++ 163. Missing Ranges.java
@@ -2,13 +2,24 @@ public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> res = new ArrayList<>();
         for(int num: nums) {
-            if(lower == num - 1) res.add(lower + "");
-            else if(lower < num - 1) res.add(lower + "->" + (num - 1));
+            // if num is MIN, num - 1 will be MAX
+            if(num == Integer.MIN_VALUE) {
+                lower = num + 1;
+                continue;
+            }
+            
+            if(lower < num - 1) res.add(lower + "->" + (num - 1));
+            else if(lower == num - 1) res.add(lower + "");
+            
             lower = num + 1;
         }
         
+        // if the last num is MAX, num + 1 will be MIN
+        if(lower == Integer.MIN_VALUE) return res;
+        
         if(lower == upper) res.add(lower + "");
         else if(lower < upper) res.add(lower + "->" + upper);
+        
         return res;
     }
 }
