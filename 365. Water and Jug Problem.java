4aa7608310c230facdcd7be636457caa33d67457
Tao Xie <taoxie@usc.edu>
2016-10-24 16:37:33 -0700

    1024

--- 365. Water and Jug Problem.java
+++ 365. Water and Jug Problem.java
@@ -1,4 +1,68 @@
 // https://leetcode.com/discuss/110478/math-solution-java-solution
+// BFS Solution, result in TLE
+public class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(x + y < z) return false;
+        if(x > y) {
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+        Queue<State> states = new ArrayDeque<>();
+        Set<State> visited = new HashSet<>();
+
+        // initial state
+        State init = new State(0, 0);
+        states.offer(init);
+        visited.add(init);
+
+        while(!states.isEmpty()) {
+            State curr = states.poll();
+            if(curr.a + curr.b == z) return true;
+
+            // fill jug1
+            Queue<State> queue = new ArrayDeque<>();
+            queue.offer(new State(x, curr.b));      									// fill jug 1
+            queue.offer(new State(0, curr.b));      									// empty jug1
+            queue.offer(new State(curr.a, y));      									// fill jug 2
+            queue.offer(new State(curr.a, 0));      									// empty jug2
+            queue.offer(new State(Math.min(curr.a + curr.b, x),
+                    curr.a + curr.b < x ? 0 : curr.b - (x - curr.a)));      			// pour all water from jug2 to jug1
+            queue.offer(new State(curr.a + curr.b < y ? 0 : curr.a - (y - curr.b),		
+                    Math.min(curr.a + curr.b, y)));										// pour all water from jug1 to jug2
+
+            for(State tmp: queue) {
+                if(visited.contains(tmp)) continue;
+                states.offer(tmp);
+                visited.add(tmp);
+            }
+        }
+        return false;
+    }
+
+
+    class State {
+        public int a, b;
+
+        public State(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int hashCode() {
+            return 31 * a + b;
+        }
+
+        public boolean equals(Object o) {
+            State other = (State)o;
+            return this.a == other.a && this.b == other.b;
+        }
+    }
+}
+
+// Math Solution
+/*
 public class Solution {
     public boolean canMeasureWater(int x, int y, int z) {
         if(x + y < z) return false;
@@ -12,3 +76,4 @@ public class Solution {
         return GCD(b, a % b);
     }
 }
+*/
