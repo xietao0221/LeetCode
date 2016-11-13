967c84d71149e2597bd9a6480482e7eb9f759e44
Tao Xie <taoxie@usc.edu>
2016-11-13 11:29:23 -0800

    1113

--- 278. First Bad Version.java
+++ 278. First Bad Version.java
@@ -1,6 +1,23 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n, middle = 0, res = 0;
+        while(left <= right) {
+            middle = left + (right - left) / 2;
+            if(isBadVersion(middle)) {
+                res = middle;
+                right = middle - 1;    
+            } else {
+                left = middle + 1;
+            }
+        }
+        return res;
+    }
+}
+
 // similar to '34. Search for a Range'
+/*
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left = 1, right = n, middle = 0;
@@ -12,3 +29,4 @@ public class Solution extends VersionControl {
         return left;
     }
 }
+*/
